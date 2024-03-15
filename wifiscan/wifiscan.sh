e07ceb16adbb1a8ce14b6ee3883e47451ed5f38f
SuperAtraction <superatraction@proton.me>
2024-03-15 20:01:31 +0100

    Update wifiscan.sh

--- wifiscan/wifiscan.sh
+++ wifiscan/wifiscan.sh
@@ -1,8 +1,11 @@
+#!/bin/bash
+
 # Vérification de privilèges
 if [ "$(id -u)" != "0" ]; then
     echo "Ce script doit être exécuté avec des privilèges sudo."
     exit 1
 fi
+
 commands=("sudo" "nmap" "grep" "sed" "awk" "xdg-open")
 for cmd in "${commands[@]}"; do
     if ! command -v "$cmd" &> /dev/null; then
@@ -10,6 +13,7 @@ for cmd in "${commands[@]}"; do
         exit 1
     fi
 done
+
 if [ -e "nmap.sh" ]; then
     ./nmap.sh
 else
