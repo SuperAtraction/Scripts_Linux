da46f9b70fcf6c801e8a94e01a90b3f41bf156be
SuperAtraction <superatraction@proton.me>
2024-03-15 19:56:51 +0100

    Création du système wifiscan

---
+++ wifiscan/.sh
@@ -0,0 +1,17 @@
+# Vérification de privilèges
+if [ "$(id -u)" != "0" ]; then
+    echo "Ce script doit être exécuté avec des privilèges sudo."
+    exit 1
+fi
+commands=("sudo" "nmap" "grep" "sed" "awk" "xdg-open")
+for cmd in "${commands[@]}"; do
+    if ! command -v "$cmd" &> /dev/null; then
+        echo "La commande $cmd n'est pas disponible sur ce système."
+        exit 1
+    fi
+done
+if [ -e "nmap.sh" ]; then
+    ./nmap.sh
+else
+    echo "Impossible de trouver le fichier nmap.sh"
+fi
